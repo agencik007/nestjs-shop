ef9e196b814cb81b3486cb5929c17723a997547b
agencik007 <agencik007@gmail.com>
2022-08-28 20:36:44 +0200

    Created basket service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShopController } from './shop/shop.controller';
 import { ShopService } from './shop/shop.service';
+import { BasketController } from './basket/basket.controller';
+import { BasketService } from './basket/basket.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ShopController],
-  providers: [AppService, ShopService],
+  controllers: [AppController, ShopController, BasketController],
+  providers: [AppService, ShopService, BasketService],
 })
 export class AppModule {}
