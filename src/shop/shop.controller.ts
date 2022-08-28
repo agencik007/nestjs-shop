7c14da0108afdfcd31e08fbb2770c452b0c0fd10
agencik007 <agencik007@gmail.com>
2022-08-28 19:15:04 +0200

    Added mock data for testing

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
 
 @Controller('shop')
-export class ShopController {}
+export class ShopController {
+
+  @Get('/')
+  getListOfProducts() {
+    return [
+      {
+        name: 'Intel Core i5 8600k',
+        description: 'dobry procesor do gier',
+        price: 1250,
+      },
+      {
+        name: '2x16GB Crucial Fast Memory 3000MHz',
+        description: 'bardzo wydajne kości RAM do PC',
+        price: 400,
+      },
+      {
+        name: 'ADATA 1TB SSD SHX3000',
+        description: 'szybki dysk SDD do komputera',
+        price: 350,
+      },
+    ]
+  }
+
+}
