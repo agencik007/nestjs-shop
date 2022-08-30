3c814bc85fd345f8f5f0a43dc39a533a696ea47e
agencik007 <agencik007@gmail.com>
2022-08-30 08:59:38 +0200

    Added the funcionality to add to basket and validation

---
+++ src/basket/basket.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Delete, Inject, Post } from "@nestjs/common";
+import { AddProductDto } from "./dto/add-product.dto";
+import { BasketService } from "./basket.service";
+import { AddProductToBasketResponse } from "../interfaces/basket";
+
+@Controller('basket')
+export class BasketController {
+  constructor(
+    @Inject(BasketService) private basketService: BasketService,
+  ) {
+  }
+
+  @Post('/')
+  addProductToBasket(
+    @Body() item: AddProductDto,
+  ): AddProductToBasketResponse {
+    return this.basketService.add(item)
+  }
+
+}
