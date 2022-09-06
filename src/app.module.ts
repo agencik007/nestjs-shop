12e86f8ae48cde3a95051ace3274fb6087c677bf
agencik007 <agencik007@gmail.com>
2022-09-06 09:03:58 +0200

    Established connection with database and created first entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { BasketModule } from "./basket/basket.module";
 import { ShopModule } from "./shop/shop.module";
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import {dbConfig} from '../db.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: dbConfig.DB_HOST,
+      port: dbConfig.DB_PORT,
+      username: dbConfig.DB_USERNAME,
+      password: dbConfig.DB_PASSWORD,
+      database: dbConfig.DB_NAME,
+      entities: ['dist/**/**.entity{.ts,.js}'],
+      bigNumberStrings: false,
+      logging: true,
+      synchronize: true,
+    }),
     BasketModule,
     ShopModule,
     UsersModule,
