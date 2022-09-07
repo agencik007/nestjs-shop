ed6c7c1d423caa1389ebd7763c49d6f8dfdd24a5
agencik007 <agencik007@gmail.com>
2022-09-07 08:46:08 +0200

    Added options to database entites

--- src/shop/shop-item.entity.ts
+++ src/shop/shop-item.entity.ts
@@ -2,15 +2,36 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class ShopItem {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-  @Column()
+  @Column({
+    length: 60,
+  })
   name: string;
 
-  @Column()
+  // @Column({
+  //   length: 10000,
+  //   default: null,
+  //   nullable: true,
+  // })
+  // description: string | null;
+
+  @Column({
+    type: 'longtext',
+    default: '(brak)',
+  })
   description: string;
 
-  @Column()
+  @Column({
+    type: 'float',
+    precision: 6,
+    scale: 2,
+  })
   price: number;
+
+  @Column({
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
 }
