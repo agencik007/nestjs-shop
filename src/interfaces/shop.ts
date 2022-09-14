5a08bf3f4a4603694f1fb667f10e6e3b761fbc19
agencik007 <agencik007@gmail.com>
2022-09-14 14:11:02 +0200

    Updated interfaces

--- src/interfaces/shop.ts
+++ src/interfaces/shop.ts
@@ -1,7 +1,12 @@
-export interface ShopItem {
+export interface ShopItemInterface {
+  id: string;
   name: string;
   description: string;
   price: number;
 }
 
-export type GetListOfProductsResponse = ShopItem[];
+export type GetListOfProductsResponse = ShopItemInterface[];
+
+export type GetOneProductResponse = ShopItemInterface;
+
+export type CreateProductResponse = ShopItemInterface;
